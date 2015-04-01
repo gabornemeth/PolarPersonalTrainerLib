680c41fe9ae506b5685a8171f90f5a8cc79e6649
Németh Gábor <gabor.nemeth1982@gmail.com>
2015-04-01 22:12:09 +0200

    added Xamarin.Android version

--- PolarPersonalTrainerLib.Android/Properties/AssemblyInfo.cs
+++ PolarPersonalTrainerLib.Android/Properties/AssemblyInfo.cs
@@ -1,9 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PolarPersonalTrainerLib")]
-[assembly: AssemblyProduct("PolarPersonalTrainerLib")]
+[assembly: AssemblyTitle("PolarPersonalTrainerLib.Android")]
+[assembly: AssemblyProduct("PolarPersonalTrainerLib.Android")]
--- PolarPersonalTrainerLib.NET45/Properties/AssemblyInfo.cs
+++ PolarPersonalTrainerLib.NET45/Properties/AssemblyInfo.cs
@@ -6,13 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PolarPersonalTrainerLib.NET45")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PolarPersonalTrainerLib.NET45")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a60e3220-3b2b-4b62-b03f-6c9bee1f2aff")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
